a838fb34a7a97b7875296b8186b326a6a9e3de1e
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-23 02:27:21 +0530

    Add files via upload

---
+++ movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/MovieCatalogResource.java
@@ -0,0 +1,64 @@
+package io.excellence.moviecatalogservice;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("get")
+public class MovieCatalogResource 
+{
+	
+	
+	@RequestMapping("catalog/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
+		{
+		RestTemplate rt=new RestTemplate();
+		
+		
+		
+		List<Rating> ratingList= Arrays.asList(
+
+				new Rating("def",5),
+				new Rating("abc",4)
+				
+				);
+		
+		return ratingList.stream().map(rating ->
+		{
+			System.out.println("hello");
+		
+	Movie mov=rt.getForObject("http://localhost:8081/get/movie/"+rating.getMovieId(), Movie.class);
+		
+		return new CatalogItem(mov.getMovieName(),"Romance",rating.getRating());
+		
+		
+		
+		}       ).collect(Collectors.toList());
+		
+		//Get All Related MovieIds
+		
+		//Get For each movieId call info service and get details
+		
+		//put them all together 
+		
+		
+		
+		
+		
+	}}
+	
+	
+	
+	
+	
+	
+
+
---
+++ movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/Rating.java
@@ -0,0 +1,19 @@
+package io.excellence.moviecatalogservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Rating {
+	
+	
+	 private String movieId ;
+	
+	private int rating ;
+	
+	
+	
+	
+
+}
