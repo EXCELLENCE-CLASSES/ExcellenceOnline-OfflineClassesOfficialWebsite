00e9036f1f8fd9d6b8a755398ba3f158888890e8
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-23 23:47:53 +0530

    Add files via upload

---
+++ movie-rating-service-final/src/main/java/io/ratingresource/movieratingservice/Rating.java
@@ -0,0 +1,19 @@
+package io.ratingresource.movieratingservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Rating {
+	
+	
+	 private String movieId ;
+	
+	private int rating ;
+	
+	
+	
+	
+
+}
