90d127ebd37d1f863b3418cd26045151905a78f3
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-23 22:32:33 +0530

    Add files via upload

--- movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/MovieCatalogResource.java
@@ -2,10 +2,11 @@ package io.excellence.moviecatalogservice;
 
 
 import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,14 @@ import org.springframework.web.client.RestTemplate;
 public class MovieCatalogResource 
 {
 	
+	@Autowired
+	public RestTemplate rt;
+	
 	
 	@RequestMapping("catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
 		{
-		RestTemplate rt=new RestTemplate();
+		
 		
 		
 		
