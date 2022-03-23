d3eef78a85ea31e3b35f1532af5e3ddec76edd4e
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-23 23:43:50 +0530

    Add files via upload

--- movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/excellence/moviecatalogservice/MovieCatalogResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("get")
@@ -18,12 +19,21 @@ public class MovieCatalogResource
 {
 	
 	@Autowired
-	public RestTemplate rt;
+	private RestTemplate rt;
+	
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder ;
+	
+	
+	
 	
 	
 	@RequestMapping("catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
 		{
+		
+		
 		
 		
 		
@@ -38,9 +48,18 @@ public class MovieCatalogResource
 		return ratingList.stream().map(rating ->
 		{
 			System.out.println("hello");
-		
-	Movie mov=rt.getForObject("http://localhost:8081/get/movie/"+rating.getMovieId(), Movie.class);
-		
+	//Creating Movie Object By RestTemplate API call	
+	// Movie mov=rt.getForObject("http://localhost:8081/get/movie/"+rating.getMovieId(), Movie.class);
+			//Creating Movie object using webClientAPI call.
+			Movie mov=webClientBuilder
+		.build()
+		.get()
+		.uri("http://localhost:8081/get/movie/"+rating.getMovieId())
+		.retrieve()
+		.bodyToMono(Movie.class)
+		.block();
+			
+			
 		return new CatalogItem(mov.getMovieName(),"Romance",rating.getRating());
 		
 		
