31f24024a46847a5a123ba2db5af97899d7c1652
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-25 03:59:39 +0530

    Add files via upload

---
+++ OneToManyWithJoinColumn/src/main/java/com/example/demo/model/ExcellenceClassesModel.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="exc_cls_model")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExcellenceClassesModel {
+	
+	@Id	
+	int excellenceId ;
+	
+	
+	String excellenceName;
+	
+   //  @OneToOne(cascade=CascadeType.ALL)
+/*	@JoinColumn(name="F_K") */
+	//We are using alternative methods for joining column using @JoinTable Annotation.
+/*	@JoinTable(name="Excellence_Student",joinColumns= {@JoinColumn(name="excellence_id")},
+	inverseJoinColumns={@JoinColumn(name="student_id")})*/
+     
+     //@OneToMany Mapping Annotation Test
+	
+    @OneToMany(cascade=CascadeType.ALL)
+     @JoinColumn(name="exce_id")
+	
+	private List<ExcellenceStudentModel> excModel=new ArrayList<>(); 
+     
+     
+	
+	
+	
+	
+
+}
