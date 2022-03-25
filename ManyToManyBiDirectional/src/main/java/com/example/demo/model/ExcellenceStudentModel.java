0716ae9b5caf308e58314cdcb916b173f5f4cf94
EXCELLENCE CLASSES <aryaritesh207@gmail.com>
2022-03-25 12:54:06 +0530

    Add files via upload

---
+++ ManyToManyBiDirectional/src/main/java/com/example/demo/model/ExcellenceStudentModel.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="exc_stu_model")
+@Getter
+@Setter
+//Getter and Setter is preferable the @Data because of Circular Dependency may caused by two different Beans ie. Injecting two beans at same time . 
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExcellenceStudentModel {
+	
+	@Id	
+	private int excellenceStudentId ;
+	
+	
+	private String excellenceStudentName ;
+	//One to one bidirectional 
+	//note:Not required @JoinColumn ,because "excModel" reference Of ExcellenceClasses Model defined in ExcellenceClasses Model is already using mappedBy. 
+	//In One-To-One Bidirectional Shared primary key mapping, two tables share the same primary key. The Bidirectional relationship means navigation is possible in both direction.
+    
+	/*	@OneToOne(cascade=CascadeType.ALL,mappedBy="excModel")
+	private ExcellenceClassesModel model ; */
+	//we are using alternative for joining two entity in ExcellenceClassesModelEntity .
+	 
+	
+	         @ManyToMany(cascade=CascadeType.ALL)
+	        
+             private Set<ExcellenceClassesModel> exc=new HashSet<>() ;
+             
+             
+	
+	
+	
+}
